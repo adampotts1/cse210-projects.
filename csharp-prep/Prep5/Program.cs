8f3b485afdddad2a64b990583a0072f39b87a65b
adampotts1 <121840447+adampotts1@users.noreply.github.com>
2023-01-10 21:39:29 -0700

    Add files via upload

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = UserSquareNumber(userNumber);
+        
+        DisplayResult(userName, squareNumber);
+
+    }
+
+//Display Name Function
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome the the program! ");
+
+    }
+// PromptUserName Function
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+// PromptUserNumber Function
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+// UserNumber squared function
+    static int UserSquareNumber(int number)
+    {
+        int square = number * number;
+       
+        return square;
+    }
+// DisplayResult Funtion
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
