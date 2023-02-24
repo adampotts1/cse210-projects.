31db58e2a85ad5f584f7c8bcfc299f73fcc01e93
adampotts1 <121840447+adampotts1@users.noreply.github.com>
2023-02-24 13:52:06 -0700

    Add files via upload

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Adam Potts ", "Homemaking");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Kathleen Potts ", "Times-Tables", "1.2", "7-8");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Melody Potts ", "Research Paper", "How sleeping through class is helpful");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
